a615a16fff64e354c8877b7652850e78b5204bb4
ryu <kaigo2013.ryuuiti@gmail.com>
2025-10-04 12:52:17 +0900

    css 練習前セーブ

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   resources :todos
+  root "home#index"
+  get "home/css" => "home#css", as: :home_css
 
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  root "home#index"
-
 end
